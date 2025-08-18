e6702ad503a1a7f851afa5ee172e4d56c19b0434
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-18 14:01:08 +0800

    BREAKING CHANGE: removed the cutomize hook

--- src/components/DisplayCards.jsx
+++ src/components/DisplayCards.jsx
@@ -2,18 +2,16 @@ import { Card } from "./Card";
 import { usePokemonData } from "./PokemonData";
 
 export function DisplayCards() {
-  const pokemons = [usePokemonData("charmander"), usePokemonData("bulbasaur")];
+  const pokemon = usePokemonData("charizard");
 
   return (
     <div className='cards'>
-      {pokemons.map((pokemon) => (
-        <Card
-          key={pokemon.id}
-          onClick={() => alert("CHARMANDER")}
-          url={pokemon.url}
-          name={pokemon.name}
-        ></Card>
-      ))}
+      <Card
+        key={pokemon.id}
+        onClick={() => alert("CHARMANDER")}
+        url={pokemon.url}
+        name={pokemon.name}
+      ></Card>
     </div>
   );
 }
--- src/components/PokemonData.jsx
+++ src/components/PokemonData.jsx
@@ -1,16 +1,25 @@
-import { useFetchData } from "./CustomizeHooks";
+import { useState, useEffect } from "react";
 
 export function usePokemonData(name) {
-  const pokemon = useFetchData(
-    "https://pokeapi.co/api/v2/pokemon-form/" + name
-  );
+  const [pokemon, setPokemon] = useState("");
 
-  if (pokemon)
-    return {
-      id: pokemon.id,
-      name: pokemon.name,
-      url: pokemon.sprites.front_default,
-    };
+  const url = "https://pokeapi.co/api/v2/pokemon-form/";
+  useEffect(() => {
+    let ignore = false;
+    fetch(url + name)
+      .then((response) => response.json())
+      .then((json) => {
+        if (!ignore) setPokemon(json);
+      });
 
-  return "";
+    return () => (ignore = true);
+  }, [name]);
+
+  return pokemon
+    ? {
+        id: pokemon.id,
+        name: pokemon.name,
+        url: pokemon.sprites.front_default,
+      }
+    : {};
 }
