5d52fbffa23515bdba0138e66870a159517c0020
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-21 22:18:02 +0800

    feat: declare the states for score, best score cardsId and functionality to reset score, increment score and if the card is unique for click and display all of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,59 @@
 import "./styles/App.css";
 import { DisplayCards } from "./components/DisplayCards";
+import { useState } from "react";
+import { NavBar } from "./components/NavBar";
+import { TryAgain } from "./components/TryAgain";
 
 export function App() {
-  return <DisplayCards></DisplayCards>;
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cardsId, setCardsId] = useState([]);
+  const isWin = score === 12;
+
+  const resetScores = () => {
+    setScore(0);
+    setBestScore(0);
+  };
+
+  const incrementScore = () => {
+    const newScore = score + 1;
+    const isBestScore = score < newScore && newScore > bestScore;
+    setScore(newScore);
+
+    if (isBestScore) setBestScore(newScore);
+    if (newScore > 12) {
+      resetScores();
+    }
+  };
+
+  const isUnique = (cardId) => {
+    if (cardsId.includes(cardId)) {
+      setScore(0);
+      setCardsId([]);
+    } else {
+      setCardsId([...cardsId, cardId]);
+      incrementScore();
+    }
+  };
+
+  return (
+    <div className='container'>
+      {isWin ? (
+        <TryAgain
+          onClick={() => {
+            resetScores();
+          }}
+        ></TryAgain>
+      ) : null}
+      <NavBar score={score} bestScore={bestScore}></NavBar>
+
+      <div className='wrapper-cards'>
+        <DisplayCards isUnique={isUnique}></DisplayCards>
+      </div>
+
+      <footer>© 2025 Memowy Game. All rights reserved.</footer>
+    </div>
+  );
 }
 
 export default App;
