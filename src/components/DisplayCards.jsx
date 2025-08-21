8576032a2c9c0cf9827e5278990f11db54d88509
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-21 22:15:47 +0800

    feat: display the cards and each card should have functionality to increment score and shuffle the cards

--- src/components/DisplayCards.jsx
+++ src/components/DisplayCards.jsx
@@ -1,20 +1,46 @@
 import { Card } from "./Card";
-import { useState } from "react";
 import { usePokemonData } from "./PokemonData";
+import { useEffect, useState } from "react";
+import { Fragment } from "react";
 
-export function DisplayCards() {
-  const [pokemons, setPokemons] = useState(usePokemonData());
+export function DisplayCards({ isUnique }) {
+  const initialPokemons = usePokemonData();
+
+  const [pokemons, setPokemons] = useState(initialPokemons);
+
+  useEffect(() => {
+    setPokemons(initialPokemons);
+  }, [initialPokemons]);
+
+  const shufflePokemons = () => {
+    const newArr = [...pokemons];
+    const shuffledArray = [];
+
+    while (newArr.length !== 0) {
+      const index = Math.floor(Math.random() * newArr.length);
+      shuffledArray.push(newArr[index]);
+      newArr.splice(index, 1);
+    }
+
+    setPokemons(shuffledArray);
+  };
 
   return (
     <div className='cards'>
-      {pokemons.map((pokemon) => (
-        <Card
-          key={pokemon.id}
-          onClick={() => alert(pokemon.name)}
-          url={pokemon.url}
-          name={pokemon.name}
-        ></Card>
-      ))}
+      {pokemons
+        ? pokemons.map((pokemon) => (
+            <Fragment key={pokemon.id}>
+              <Card
+                onClick={() => {
+                  shufflePokemons();
+                  isUnique(pokemon.id);
+                }}
+                url={pokemon.url}
+                name={pokemon.name}
+              ></Card>
+            </Fragment>
+          ))
+        : "LOADING..."}
     </div>
   );
 }
